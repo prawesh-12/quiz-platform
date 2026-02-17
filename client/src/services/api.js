8d92ad25ef68ad96a1918e8f5447579e62f2b0f7
Prawesh Mandal <praweshm45@gmail.com>
2026-02-17 20:27:14 +0530

    fixes: api error

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -5,7 +5,7 @@ import { pushToast } from "@/hooks/useToast";
 const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 export const api = axios.create({
-  baseURL: API_BASE_URL
+  baseURL: `${API_BASE_URL.replace(/\/$/, "")}/api`
 });
 
 api.interceptors.request.use((config) => {
